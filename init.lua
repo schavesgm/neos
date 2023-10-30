dbe4f192fded57547a8f89615d9973b30e910e5f
Sergio Chaves García-Mascaraque <s.garciamascaraque@instadeep.com>
2023-10-30 19:57:02 +0000

    feat(bootstrap): add initial bootstrap configuration

--- init.lua
+++ init.lua
@@ -5,3 +5,6 @@ local config_path = path:match("(.*[/\\])"):sub(1, -2)
 if not vim.tbl_contains(vim.opt.rtp:get(), config_path) then
 	vim.opt.rtp:append(config_path)
 end
+
+-- Bootstrap configuration
+require("bootstrap"):init(config_path)
---
+++ lua/bootstrap.lua
@@ -0,0 +1,54 @@
+-- Module used to bootstrap the configuration of gruvim
+local M = {}
+
+-- Minimum neovim version supported by the configuration
+local MINIMUM_NVIM_VERSION = "0.9"
+
+---Assert the NeoVim configuration is greater than a pre-defined value
+local function _assert_miminum_nvim_version()
+    local nvim_version = string.format("nvim-%s", MINIMUM_NVIM_VERSION)
+    if vim.fn.has(nvim_version) ~= 1 then
+        local error_message = string.format(
+            "Please upgrade your Neovim base installation. Requires v%s+",
+            MINIMUM_NVIM_VERSION
+        )
+        vim.notify(error_message, vim.log.levels.WARN)
+        vim.wait(5000, function() return false end)
+        vim.cmd "cquit"
+    end
+end
+
+---Bootstrap lazy-nvim plugin manager: https://github.com/folke/lazy.nvim
+local function _bootstrap_lazy_nvim()
+	local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+	if not vim.loop.fs_stat(lazypath) then
+        vim.notify("🥾 Bootstrapping lazy plugin manager")
+	    vim.fn.system({
+	        "git",
+	        "clone",
+	        "--filter=blob:none",
+	        "--single-branch",
+	        "https://github.com/folke/lazy.nvim.git",
+	        lazypath,
+	    })
+	end
+	vim.opt.runtimepath:prepend(lazypath)
+end
+
+---Initialise the configuration.
+---@param path_to_config string; path to configuration to be saved as global
+function M:init(path_to_config)
+    _assert_miminum_nvim_version()
+    _bootstrap_lazy_nvim()
+
+    _G.configuration = {
+        path_to_config = path_to_config
+    }
+end
+
+---Reload the neovim configuration
+function M:reload()
+    M:init(_G.configuration.path_to_config)
+end
+
+return M
