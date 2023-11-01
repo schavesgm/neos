f3f320588a3bce28c167c75d3273d6b1578ce0f3
Sergio Chaves García-Mascaraque <s.garciamascaraque@instadeep.com>
2023-11-01 12:02:38 +0000

    refactor(init): change symbol name

--- init.lua
+++ init.lua
@@ -1,10 +1,10 @@
 -- Add the current configuration path to runtimepath
 local path = debug.getinfo(1, "S").source:sub(2)
-local config_path = path:match("(.*[/\\])"):sub(1, -2)
+local path_to_config = path:match("(.*[/\\])"):sub(1, -2)
 
-if not vim.tbl_contains(vim.opt.rtp:get(), config_path) then
-    vim.opt.rtp:append(config_path)
+if not vim.tbl_contains(vim.opt.rtp:get(), path_to_config) then
+    vim.opt.rtp:append(path_to_config)
 end
 
 -- Bootstrap configuration
-require("bootstrap"):init(config_path)
+require("bootstrap"):init(path_to_config)
