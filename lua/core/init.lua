64c90cf28e2952f7afe25bf948f96bbb5cfc04b0
Sergio Chaves García-Mascaraque <s.garciamascaraque@instadeep.com>
2023-11-02 16:30:53 +0000

    feat(core): initialise entry point

---
+++ lua/core/init.lua
@@ -0,0 +1,6 @@
+---Module containing all core implementations of the configuration
+return {
+    options = require("core.options"),
+    keymaps = require("core.keymaps"),
+    autocmds = require("core.autocmds"),
+}
