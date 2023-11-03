cb4f6dc8e06566b164bd8c40798d618efde0e6a6
Sergio Chaves García-Mascaraque <s.garciamascaraque@instadeep.com>
2023-11-03 14:31:20 +0000

    feat(scripts): pass neovim executable name from command line

--- scripts/check_config.sh
+++ scripts/check_config.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-nvim.appimage -u init.lua -V1output.log +:q
+NEOVIM_EXEC=${1}
+
+${NEOVIM_EXEC} -u init.lua -V1output.log +:q
 CONTAINS_ERROR=$(grep -q "Error detected while processing" output.log; echo $?)
 rm ./output.log
 if [ "${CONTAINS_ERROR}" -eq "0" ]; then
