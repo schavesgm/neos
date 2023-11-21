b8976c8e62023f1eae74af4794e0301c86858ff7
Sergio Chaves García-Mascaraque <s.garciamascaraque@instadeep.com>
2023-11-21 16:26:27 +0000

    feat(plugins): add support for autocompletion and snippets

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,2 +1,32 @@
 ---Module containing all plugin definitions
-return {}
+return {
+    -- Autocompletion and snippets
+    {
+        "rafamadriz/friendly-snippets",
+        event = "InsertEnter",
+    },
+    {
+        "saadparwaiz1/cmp_luasnip",
+        dependencies = "LuaSnip",
+    },
+    {
+        "hrsh7th/cmp-nvim-lua",
+        dependencies = "LuaSnip"
+    },
+    {
+        "hrsh7th/cmp-nvim-lsp",
+        dependencies = "LuaSnip",
+    },
+    {
+        "hrsh7th/cmp-buffer",
+        dependencies = "LuaSnip"
+    },
+    {
+        "hrsh7th/cmp-path",
+        dependencies = "LuaSnip",
+    },
+    {
+        "kdheepak/cmp-latex-symbols",
+        dependencies = "LuaSnip"
+    },
+}
---
+++ lua/plugins/luasnip.lua
@@ -0,0 +1,30 @@
+local M = {
+    "L3MON4D3/LuaSnip",
+    dependencies = {"nvim-cmp", "friendly-snippets"},
+}
+
+function M.config()
+    local luasnip = _G.neos.base.safely_load("luasnip", vim.log.levels.WARN)
+    if not luasnip then return end
+
+    luasnip.config.set_config({
+        history = true,
+        updateevents = "TextChanged,TextChangedI",
+    })
+
+    require("luasnip.loaders.from_vscode").lazy_load()
+    require("luasnip.loaders.from_lua").lazy_load({paths = "./snippets"})
+
+    vim.api.nvim_create_autocmd("InsertLeave", {
+        callback = function()
+            if
+                luasnip.session.current_nodes[vim.api.nvim_get_current_buf()]
+                and not require("luasnip").session.jump_active
+            then
+                luasnip.unlink_current()
+            end
+        end,
+    })
+end
+
+return M
