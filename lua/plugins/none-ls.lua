d161ef8186d5d689fe1de2eafe17a7b26e252e2e
Sergio Chaves García-Mascaraque <s.garciamascaraque@instadeep.com>
2023-11-22 14:03:31 +0000

    feat(plugins): add support for none-ls

---
+++ lua/plugins/none-ls.lua
@@ -0,0 +1,31 @@
+local M = {
+    "nvimtools/none-ls.nvim",
+    dependencies = { "nvim-lua/plenary.nvim", "nvim-lua/popup.nvim" },
+}
+
+function M.config()
+    local none_ls = _G.neos.base.safely_load("null-ls", vim.log.levels.WARN)
+    if not none_ls then
+        return
+    end
+
+    none_ls.setup({
+        sources = {
+            -- Filetype: Lua
+            none_ls.builtins.formatting.stylua,
+
+            -- Filetype: Python
+            none_ls.builtins.diagnostics.ruff,
+            none_ls.builtins.diagnostics.mypy.with({
+                extra_args = {
+                    "--show-column-numbers",
+                    "--disallow-untyped-defs",
+                    "--check-untyped-defs",
+                    "--ignore-missing-imports",
+                },
+            }),
+        },
+    })
+end
+
+return M
