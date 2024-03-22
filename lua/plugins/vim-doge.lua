499b1f432be120a59412b0fec1ea67ad57927df5
Sergio Chaves García-Mascaraque <s.garciamascaraque@instadeep.com>
2024-03-22 17:33:04 +0000

    feat(plugins): add support for `vim-doge` plugin

---
+++ lua/plugins/vim-doge.lua
@@ -0,0 +1,15 @@
+local M = {
+    "kkoomen/vim-doge",
+    build = ":call doge#install()",
+}
+
+function M.config()
+    -- Setup the default Python configuration
+    vim.g["doge_doc_standard_python"] = "google"
+    vim.g["doge_python_settings"] = {
+        single_quotes = 0,
+        omit_redundant_param_types = 0
+    }
+end
+
+return M
