5e14b9fa3b6026432d6496a4fc881d6d47b09fe4
Sergio Chaves García-Mascaraque <s.garciamascaraque@instadeep.com>
2024-04-03 10:51:28 +0100

    feat(plugins): add `neogit` plugin

---
+++ lua/plugins/neogit.lua
@@ -0,0 +1,12 @@
+local M = {
+    "NeogitOrg/neogit",
+    dependencies = {
+        "nvim-lua/plenary.nvim",
+        "sindrets/diffview.nvim",
+        "nvim-telescope/telescope.nvim",
+    },
+    config = true,
+    cmd = { "Neogit" },
+}
+
+return M
