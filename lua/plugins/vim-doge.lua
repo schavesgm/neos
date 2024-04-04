7987406e48fa2d16a99b2a6ce435414350ecc18b
Sergio Chaves García-Mascaraque <s.garciamascaraque@instadeep.com>
2024-04-04 09:59:43 +0100

    refactor(plugins): format code to match `stylua`

--- lua/plugins/vim-doge.lua
+++ lua/plugins/vim-doge.lua
@@ -8,7 +8,7 @@ function M.config()
     vim.g["doge_doc_standard_python"] = "google"
     vim.g["doge_python_settings"] = {
         single_quotes = 0,
-        omit_redundant_param_types = 0
+        omit_redundant_param_types = 0,
     }
 end
 
