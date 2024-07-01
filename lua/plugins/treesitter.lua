9646676621f0020957117fe02cbf6291f1f672fe
Sergio Chaves García-Mascaraque <sergiozteskate@gmail.com>
2024-07-01 11:52:19 +0100

    feat(plugins): enhance treesitter experience

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -32,6 +32,17 @@ function M.config()
             use_languagetree = true,
         },
 
+        -- Treesitter incremental selection module
+        incremental_selection = {
+            enable = true,
+            keymaps = {
+                init_selection = "<C-space>",
+                node_incremental = "<C-space>",
+                scope_incremental = false,
+                node_decremental = "<bs>",
+            },
+        },
+
         -- Treesitter indentation module
         indent = {
             enable = true,
@@ -48,10 +59,65 @@ function M.config()
                 enable = true,
                 lookahead = true,
                 keymaps = {
-                    ["ia"] = "@parameter.inner",
-                    ["aa"] = "@parameter.outer",
-                    ["ic"] = "@comment.inner",
-                    ["ac"] = "@comment.outer",
+                    -- You can use the capture groups defined in textobjects.scm
+                    ["a="] = {
+                        query = "@assignment.outer",
+                        desc = "Select outer part of an assignment",
+                    },
+                    ["i="] = {
+                        query = "@assignment.inner",
+                        desc = "Select inner part of an assignment",
+                    },
+                    ["l="] = {
+                        query = "@assignment.lhs",
+                        desc = "Select left hand side of an assignment",
+                    },
+                    ["r="] = {
+                        query = "@assignment.rhs",
+                        desc = "Select right hand side of an assignment",
+                    },
+
+                    ["aa"] = {
+                        query = "@parameter.outer",
+                        desc = "Select outer part of a parameter/argument",
+                    },
+                    ["ia"] = {
+                        query = "@parameter.inner",
+                        desc = "Select inner part of a parameter/argument",
+                    },
+
+                    ["ai"] = {
+                        query = "@conditional.outer",
+                        desc = "Select outer part of a conditional",
+                    },
+                    ["ii"] = {
+                        query = "@conditional.inner",
+                        desc = "Select inner part of a conditional",
+                    },
+
+                    ["al"] = { query = "@loop.outer", desc = "Select outer part of a loop" },
+                    ["il"] = { query = "@loop.inner", desc = "Select inner part of a loop" },
+
+                    ["af"] = {
+                        query = "@call.outer",
+                        desc = "Select outer part of a function call",
+                    },
+                    ["if"] = {
+                        query = "@call.inner",
+                        desc = "Select inner part of a function call",
+                    },
+
+                    ["am"] = {
+                        query = "@function.outer",
+                        desc = "Select outer part of a method/function definition",
+                    },
+                    ["im"] = {
+                        query = "@function.inner",
+                        desc = "Select inner part of a method/function definition",
+                    },
+
+                    ["ac"] = { query = "@class.outer", desc = "Select outer part of a class" },
+                    ["ic"] = { query = "@class.inner", desc = "Select inner part of a class" },
                 },
             },
         },
