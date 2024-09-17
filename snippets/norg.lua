6eccf8c410af750c1cbedacee109ae7822a8918e
Sergio Chaves García-Mascaraque <sergiozteskate@gmail.com>
2024-09-17 21:04:04 +0100

    feat(snippets) ✨: modify inline code and math snippets

--- snippets/norg.lua
+++ snippets/norg.lua
@@ -2,7 +2,8 @@ local snippets = require("luasnip")
 local format = require("luasnip.extras.fmt").fmt
 
 local entries = {
-    snippets.s("inlinemath", format("$|{}|$", { snippets.i(1, "your_maths") })),
+    snippets.s("imath", format("$|{}|$", { snippets.i(1, "your_maths") })),
+    snippets.s("icode", format("$`{}`$", { snippets.i(1, "your_maths") })),
 }
 
 return entries
