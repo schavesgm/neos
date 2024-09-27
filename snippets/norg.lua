fbd22210aec95c69772a950c1dbb692daad3fd91
Sergio Chaves García-Mascaraque <sergiozteskate@gmail.com>
2024-09-27 18:17:19 +0100

    fix(snippets): 🐛 correct error in snippet default value

--- snippets/norg.lua
+++ snippets/norg.lua
@@ -3,7 +3,7 @@ local format = require("luasnip.extras.fmt").fmt
 
 local entries = {
     snippets.s("imath", format("$|{}|$", { snippets.i(1, "your_maths") })),
-    snippets.s("icode", format("$`{}`$", { snippets.i(1, "your_maths") })),
+    snippets.s("icode", format("`{}`", { snippets.i(1, "your_code") })),
 }
 
 return entries
